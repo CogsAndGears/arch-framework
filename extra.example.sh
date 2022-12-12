590ae0b8822ede0383dc6cfdea4c3f8332a56b2e
cogs <cogs@springless.com>
2022-12-11 23:41:14 -0700

    Add /etc config files

--- extra.example.sh
+++ extra.example.sh
@@ -19,3 +19,15 @@ EXTRA_UTILITIES=\
 	robotfindskitten
 
 $INSTALL ${DESKTOP_REQ} ${EXTRA_UTILITIES}
+
+# Make Dvorak the X11 default keyboard input
+cp ${OS_INSTALL_DIR}/conf/xorg.conf.d/* /etc/X11/xorg.conf.d/.
+
+######
+# Configure startx to start xfce4
+cp /etc/xdg/xfce4/xinitrc /home/${OS_USERNAME}/.xinitrc
+
+####
+# Disable auto-light detection/brightness adjustment in favour of maniual adjustment. Currently
+# it seems to be a one or the other situation
+cp ${OS_INSTALL_PATH}/conf/modprobe.d/* /etc/modprobe.d/.
